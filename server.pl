9e8c4431b5d690272bf359e26d58ee8bd2c189f2
Sjoerd Dost <deosjr@gmail.com>
2025-03-26 19:58:12 +0100

    contact page scaffolding

--- server.pl
+++ server.pl
@@ -8,8 +8,31 @@
                 []).
 :- http_handler(root(contacts), contacts, []).
 
-contacts(_Request) :-
-    reply_html_page(
-        title('Hypermedia systems'),
-        [ h1('Hello world!')
-        ]).
+contacts(Request) :-
+    http_parameters(Request, [ q(Search, [optional(true)]) ]),
+    ( var(Search) -> 
+        Contacts='todo findall'
+        ;
+        Contacts='todo search'
+    ),
+    layout_head_template(Head),
+    index_template(Contacts, Body),
+    reply_html_page(Head, Body).
+
+%% templates
+
+layout_head_template([
+    title('Contact App'),
+    link([rel(stylesheet), href("https://the.missing.style/v0.2.0/missing.min.css")]),
+    script([src("https://unpkg.com/htmx.org@1.8.0")], [])
+]).
+
+layout_template(Content) --> 
+    [header([h1([div('CONTACTS.APP'), div('A Demo Contacts Application')])])], Content.
+
+index_template(Contacts, [main(Out)]) :-
+    Content = [
+        h2('Contacts'),
+        div(Contacts)
+    ],
+    phrase(layout_template(Content), Out).
